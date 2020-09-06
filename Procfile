c85d0dbdd3df42db5f65f8009b233e780c444d25
Hamza <hamzaelharsi123@gmail.com>
2020-09-06 17:43:25 +0100

    Procfile for Heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: vendor/bin/heroku-php-apache2 public/
+worker: php artisan queue:work --sleep=0 --timeout=60000
--- resources/views/actors/index.blade.php
+++ resources/views/actors/index.blade.php
@@ -3,14 +3,16 @@
 @section('content')
 <div class="container-fluid" >
     <div class="row justify-content-center">
-        @foreach ($actors as $actor)
+        @forelse ($actors as $actor)
             <div class="col-lg-2 col-md-3 col-sm-4 col-10 text-center">
                 <a href="{{ route('actors.show',$actor->slug) }}" class="text-dark" >
                     <img src="{{ $actor->photo }}" class="rounded" alt="{{ $actor->name }}'s photo'" >
                     <h5 class="movie-title">{{ $actor->name }}</h5>
                 </a>
             </div>
-        @endforeach
+        @empty
+        <h1>No actors</h1>
+        @endforelse
 
 
     </div>
